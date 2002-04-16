f388ef66b2267dfa361c04426dc9e925ff5a2226
steve <steve>
2002-04-16 22:22:43 +0000

    Generate OUTPUT chain in the nat table.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 PREFIX=${PREFIX:-""}
 BINDIR=${BINDIR:-"$PREFIX/usr/local/bin"}
-MANDIR=${MANDIR:-"$PREFIX/usr/local/man/man8"}
+MANDIR=${MANDIR:-"$PREFIX/usr/local/share/man/man8"}
 INITDIR=${INITDIR:-"$PREFIX/etc/rc.d/init.d"}
 
 set -e
@@ -10,5 +10,10 @@ set -x
 
 install -b -o root -g root genfw $BINDIR/genfw
 install -b -o root -g root firewall.init $INITDIR/firewall
+
+pod2man genfw > genfw.8
+gzip -9 genfw.8
+
+install -b -o root -g root genfw.8.gz $MANDIR/genfw.8.gz
 
 chkconfig --add firewall
