8bf669b9f0e992fb13cade84e0b55034a1902103
steve <steve>
2002-07-30 02:46:40 +0000

    Add support for loading firewall modules in the init script. Start working on support for better packaging.

--- install.sh
+++ install.sh
@@ -4,12 +4,20 @@ PREFIX=${PREFIX:-""}
 BINDIR=${BINDIR:-"$PREFIX/usr/local/bin"}
 MANDIR=${MANDIR:-"$PREFIX/usr/local/share/man/man8"}
 INITDIR=${INITDIR:-"$PREFIX/etc/rc.d/init.d"}
+CONFIGDIR=${CONFIGDIR:-"$PREFIX/etc/sysconfig/genfw"}
 
 set -e
 set -x
 
 install -b -o root -g root genfw $BINDIR/genfw
 install -b -o root -g root firewall.init $INITDIR/firewall
+
+mkdir -p $CONFIGDIR
+for file in rules modules ; do
+    if [ ! -f $CONFIGDIR/$file ] ; then
+        touch $CONFIGDIR/$file
+    fi
+done
 
 pod2man genfw > genfw.8
 gzip -9 genfw.8
