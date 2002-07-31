c1d507e7112727b677ace9db39593bcf39d1bded
steve <steve>
2002-07-31 03:55:44 +0000

    MAN8DIR, not MANDIR, dammit.

--- install.sh
+++ install.sh
@@ -24,9 +24,9 @@ done
 pod2man genfw > genfw.8
 if [ -z "$INSTPREFIX" ] ; then
     gzip -9 genfw.8
-    install -b -o root -g root genfw.8.gz $MANDIR/genfw.8.gz
+    install -b -o root -g root genfw.8.gz $MAN8DIR/genfw.8.gz
 else
-    install -b -o root -g root genfw.8 $MANDIR/genfw.8
+    install -b -o root -g root genfw.8 $MAN8DIR/genfw.8
 fi
 
 if [ -z "$INSTPREFIX" ] ; then
